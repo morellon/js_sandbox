d2652b8c3e02027de457b84b9734b99e78c83d78
Thiago Morello <thiago.morello@locaweb.com.br>
2010-10-29 18:06:41 -0200

    Some refactoring

--- linechart_files/chart.js
+++ linechart_files/chart.js
@@ -1,13 +1,13 @@
-function drawLegend(r, x, y, opts) {
+function drawLegend(raphael, x, y, opts) {
 	lines = opts.chart.lines
-	r.g.text(x+15 , y+15, opts.title).attr({"text-anchor": "start"});
+	raphael.g.text(x+15 , y+15, opts.title).attr({"text-anchor": "start"});
 	for(var i=0; i < lines.length; i++) {
 		color = lines[i].attrs.stroke;
 		label_y = y + 15 * (i+2);
-		r.circle(x + 20, label_y, 5).attr({"stroke": color, "fill": color});
-		r.g.text(x + 30, label_y, opts.labels[i]).attr({"text-anchor": "start"});
+		raphael.circle(x + 20, label_y, 5).attr({"stroke": color, "fill": color});
+		raphael.g.text(x + 30, label_y, opts.labels[i]).attr({"text-anchor": "start"});
 	}
-	r.rect(x, y, 300, 15 * (lines.length + 2) );
+	raphael.rect(x, y, 300, 15 * (lines.length + 2) );
 }
 
 function manipulateAxis(axis, transform) {
@@ -17,19 +17,7 @@ function manipulateAxis(axis, transform) {
 	}
 }
 
-function drawChart(holder, opts) {
-	var r = Raphael(holder);
-    r.g.txtattr.font = "12px 'Fontin Sans', Fontin-Sans, sans-serif";
-
-    r.g.text(100, 20, opts.title);
-	var width = 680;
-	var height = 400;
-	var time = opts.time
-	var values = opts.values;
-
-    var chart = r.g.linechart(20, 20, width, height, time, values, {shade: true, nostroke: false, axis: "0 0 1 1", symbol: "o", smooth: true});
-    chart.symbols.attr({r: 4});
-	
+function setSymbolCallbacks(raphael, chart, time, values) {
 	for (var i=0; i < chart.symbols.length; i++ ) {
 		for (var j=0; j < chart.symbols[i].length; j++ ) {
 			(function(graphIndex, valueIndex) {
@@ -38,13 +26,13 @@ function drawChart(holder, opts) {
 						this.attr("r", 6);
 						color = this.attrs.fill;
 
-						var label = r.set();
-						label.push(r.text(60, 12, values[graphIndex][valueIndex] + " hits").attr({font: '12px Helvetica, Arial', fill: "#fff"}));
+						var label = raphael.set();
+						label.push(raphael.text(60, 12, values[graphIndex][valueIndex] + " hits").attr({font: '12px Helvetica, Arial', fill: "#fff"}));
 						date = new Date(time[valueIndex] * 1000)
-					    label.push(r.text(60, 27, date).attr({font: '10px Helvetica, Arial', fill: "#fff"}));
+					    label.push(raphael.text(60, 27, date).attr({font: '10px Helvetica, Arial', fill: "#fff"}));
 						this.label = label;
 
-						this.tag = r.popup(this.attrs.cx + 5, this.attrs.cy, label, "right").attr({fill: "#000", stroke: "#666", "stroke-width": 2, "fill-opacity": .7});
+						this.tag = raphael.popup(this.attrs.cx + 5, this.attrs.cy, label, "right").attr({fill: "#000", stroke: "#666", "stroke-width": 2, "fill-opacity": .7});
 					},
 					function () {
 						this.attr("r", 4);
@@ -55,6 +43,23 @@ function drawChart(holder, opts) {
 			})(i, j)
 		}
 	}
+}
+
+function drawChart(holder, opts) {
+	var r = Raphael(holder);
+    // r.g.txtattr.font = "12px 'Fontin Sans', Fontin-Sans, sans-serif";
+
+	var width = opts.width;
+	var height = opts.height;
+	var time = opts.time
+	var values = opts.values;
+	
+	r.g.text(100, 20, opts.title);
+
+    var chart = r.g.linechart(20, 20, width, height, time, values, {shade: true, nostroke: false, axis: "0 0 1 1", symbol: "o", smooth: true});
+    chart.symbols.attr({r: 4});
+
+	setSymbolCallbacks(r, chart, time, values);
 	
 	manipulateAxis(chart.axis[0], function (text) {
 		date = new Date(parseInt(text) * 1000);
