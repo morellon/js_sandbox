55ef97e954a2b57b279b1e74baf8d50da0c77f48
Thiago Morello <morellon@gmail.com>
2010-11-03 22:43:45 -0200

    Added some recursion for creating graphics from selection

--- linechart_files/chart.js
+++ linechart_files/chart.js
@@ -7,148 +7,149 @@ Date.formatted_time = function (text) {
 	return day + "/" + month + "\n" + hours + ":" + minutes;
 }
 
-function drawLegend(x, y, opts) {
-	var chart = opts.chart,
-			lines = opts.chart.lines,
-			raphael = lines[0].paper,
-			legend = raphael.set();
-	
-	if (opts.title) {
-		legend.push(raphael.g.text(x + 15 , y + 15, opts.title).attr({"text-anchor": "start"}));
-	}
-	
-	for(var i = 0; i < lines.length; i++) {
-		var color = lines[i].attrs.stroke;
-		var label_y = y + 15 * (i+2);
-		var legendItem = raphael.set();
-		legendItem.push(raphael.circle(x + 20, label_y, 5).attr({"stroke": color, "fill": color}));
-		legendItem.push(raphael.g.text(x + 30, label_y, opts.labels[i]).attr({"text-anchor": "start"}));
-		(function(graphIndex) {
-			var line = lines[graphIndex];
-			legendItem.hover(
-				function () {
-					line.attr({"stroke-width": 4});
-				},
-				function () {
-					line.attr({"stroke-width": 2});
-				}
-			);
-		})(i);
-		
-		legend.push(legendItem);
-	}
-	
-	legend.push(raphael.rect(x, y, 300, 15 * (lines.length + 2)));
-	chart.legend = legend;
-	chart.push(legend);
-	
-	return legend;
-}
+Raphael.fn.g.extendedlinechart = function(opts) {
+	function drawLegend(x, y, opts) {
+		var chart = opts.chart,
+				lines = opts.chart.lines,
+				raphael = lines[0].paper,
+				legend = raphael.set();
 
-function defineChartArea(x, y, opts) {
-	var width = opts.width;
-	var height = opts.height;
-	var chart = opts.chart;
-	var div = chart.div;
-	var raphael = chart.lines[0].paper;
-	
-	var column = raphael.rect(0, y, 1, height).attr({fill: "#fff", "opacity": 0});
-	var chartArea = raphael.rect(x, y, width, height).attr({fill: "#fff", "opacity": 0});
-	chart.columnX = column;
-	chart.area = chartArea;
-	chart.push(chartArea, column);
-	
-	chartArea.mousemove(function (e) {
-		column.attr({opacity: 0.5, x: (e.pageX - div.offsetLeft)});
-	});
-	chartArea.mouseout(function () {
-		column.attr({opacity: 0, x: 0});
-	});
-	
-	chart.selection = function(f) {
-		chartArea.mousedown(function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-			if (!this.selection) {
-				this.selection = this.paper.rect(column.attrs.x, column.attrs.y, column.attrs.width, column.attrs.height).attr({stroke: "#404", fill: "#c0c", opacity: 0.3});
-				// if selection rectangle is in front, the events won't work
-				this.selection.toBack();
-				
-				this.selection.startValue = function () {
-					var xStep = (chart.xValues[chart.xValues.length - 1] - chart.xValues[0])/width;
-					return ((this.attrs.x - x) * xStep) + chart.xValues[0];
-				}
-				this.selection.endValue = function () {
-					var xStep = (chart.xValues[chart.xValues.length - 1] - chart.xValues[0])/width;
-					return ((this.attrs.x - x + this.attrs.width) * xStep) + chart.xValues[0];
-				}
-				
-				this.selection.start = column.attrs.x;
-			}
-		}).mousemove(function() {
-			if (this.selection) {
-				var width = column.attrs.x - this.selection.start;
-				if (width >= 0) {
-					this.selection.attr({x: this.selection.start, width: width});
-				} else {
-					this.selection.attr({x: column.attrs.x, width: -width});
-				}
-					
-			}
-		}).mouseup(function() {
-			if (this.selection) {
-				f && f(this.selection);
-				this.selection.remove();
-				this.selection = undefined;
-			}
-		});
-	}
-	
-	return chartArea;
-}
+		if (opts.title) {
+			legend.push(raphael.g.text(x + 15 , y + 15, opts.title).attr({"text-anchor": "start"}));
+		}
 
-function overwriteAxis(axis, transform) {
-	var texts = axis.text;
-	for(var i=0; i < texts.length; i++) {
-		var text = texts[i];
-		var new_text = (transform || Date.formatted_time)(text.attrs.text);
-		text.attr("text", new_text);
-		text.translate(0, 5);
-	}
-}
-
-function setSymbolCallbacks(chart, time, values, units) {
-	for (var i=0; i < chart.symbols.length; i++ ) {
-		for (var j=0; j < chart.symbols[i].length; j++ ) {
-			(function(graphIndex, valueIndex) {
-				var symbol = chart.symbols[graphIndex][valueIndex];
-				symbol.toFront();
-				symbol.hover(
-					function() {
-						var color = this.attrs.fill,
-							raphael = this.paper,
-							date = new Date(time[valueIndex] * 1000),
-							label = raphael.set();
-						
-						label.push(raphael.text(60, 12, values[graphIndex][valueIndex] + " " + units[graphIndex]).attr({font: '12px Helvetica, Arial', fill: "#fff"}));
-						label.push(raphael.text(60, 27, date).attr({font: '10px Helvetica, Arial', fill: "#fff"}));
-						this.tag = raphael.popup(this.attrs.cx + 5, this.attrs.cy, label, "right").attr({fill: "#000", stroke: "#666", "stroke-width": 2, "fill-opacity": .7});
-						this.label = label;
-						this.attr("r", 6);
+		for(var i = 0; i < lines.length; i++) {
+			var color = lines[i].attrs.stroke;
+			var label_y = y + 15 * (i+2);
+			var legendItem = raphael.set();
+			legendItem.push(raphael.circle(x + 20, label_y, 5).attr({"stroke": color, "fill": color}));
+			legendItem.push(raphael.g.text(x + 30, label_y, opts.labels[i]).attr({"text-anchor": "start"}));
+			(function(graphIndex) {
+				var line = lines[graphIndex];
+				legendItem.hover(
+					function () {
+						line.attr({"stroke-width": 4});
 					},
-					function() {
-						this.tag && this.tag.remove();
-						this.label && this.label.remove();
-						this.attr("r", 4);
+					function () {
+						line.attr({"stroke-width": 2});
 					}
 				);
-			})(i, j)
+			})(i);
+
+			legend.push(legendItem);
+		}
+
+		legend.push(raphael.rect(x, y, 300, 15 * (lines.length + 2)));
+		chart.legend = legend;
+		chart.push(legend);
+
+		return legend;
+	}
+
+	function defineChartArea(x, y, opts) {
+		var width = opts.width;
+		var height = opts.height;
+		var chart = opts.chart;
+		var div = chart.div;
+		var raphael = chart.lines[0].paper;
+
+		var column = raphael.rect(0, y, 1, height).attr({fill: "#fff", "opacity": 0});
+		var chartArea = raphael.rect(x, y, width, height).attr({fill: "#fff", "opacity": 0});
+		chart.columnX = column;
+		chart.area = chartArea;
+		chart.push(chartArea, column);
+
+		chartArea.mousemove(function (e) {
+			column.attr({opacity: 0.5, x: (e.pageX - div.offsetLeft)});
+		});
+		chartArea.mouseout(function () {
+			column.attr({opacity: 0, x: 0});
+		});
+
+		chart.selection = function(f) {
+			var selection;
+			chartArea.mousedown(function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				if (!selection) {
+					selection = this.paper.rect(column.attrs.x, column.attrs.y, column.attrs.width, column.attrs.height).attr({stroke: "#404", fill: "#c0c", opacity: 0.3});
+					// if selection rectangle is in front, the events won't work
+					selection.toBack();
+
+					selection.startValue = function () {
+						var xStep = (chart.xValues[chart.xValues.length - 1] - chart.xValues[0])/width;
+						return ((this.attrs.x - x) * xStep) + chart.xValues[0];
+					}
+					selection.endValue = function () {
+						var xStep = (chart.xValues[chart.xValues.length - 1] - chart.xValues[0])/width;
+						return ((this.attrs.x - x + this.attrs.width) * xStep) + chart.xValues[0];
+					}
+
+					selection.start = column.attrs.x;
+				}
+			}).mousemove(function() {
+				if (selection) {
+					var width = column.attrs.x - selection.start;
+					if (width >= 0) {
+						selection.attr({x: selection.start, width: width});
+					} else {
+						selection.attr({x: column.attrs.x, width: -width});
+					}
+
+				}
+			}).mouseup(function() {
+				if (selection) {
+					f && f(selection);
+					selection.remove();
+					selection = undefined;
+				}
+			});
+		}
+
+		return chartArea;
+	}
+
+	function overwriteAxis(axis, transform) {
+		var texts = axis.text;
+		for(var i=0; i < texts.length; i++) {
+			var text = texts[i];
+			var new_text = (transform || Date.formatted_time)(text.attrs.text);
+			text.attr("text", new_text);
+			text.translate(0, 5);
 		}
 	}
-}
 
-function drawChart(holder, opts) {
-	var r = Raphael(holder);
+	function setSymbolCallbacks(chart, time, values, units) {
+		for (var i=0; i < chart.symbols.length; i++ ) {
+			for (var j=0; j < chart.symbols[i].length; j++ ) {
+				(function(graphIndex, valueIndex) {
+					var symbol = chart.symbols[graphIndex][valueIndex];
+					symbol.toFront();
+					symbol.hover(
+						function() {
+							var color = this.attrs.fill,
+								raphael = this.paper,
+								date = new Date(time[valueIndex] * 1000),
+								label = raphael.set();
+
+							label.push(raphael.text(60, 12, values[graphIndex][valueIndex] + " " + units[graphIndex]).attr({font: '12px Helvetica, Arial', fill: "#fff"}));
+							label.push(raphael.text(60, 27, date).attr({font: '10px Helvetica, Arial', fill: "#fff"}));
+							this.tag = raphael.popup(this.attrs.cx + 5, this.attrs.cy, label, "right").attr({fill: "#000", stroke: "#666", "stroke-width": 2, "fill-opacity": .7});
+							this.label = label;
+							this.attr("r", 6);
+						},
+						function() {
+							this.tag && this.tag.remove();
+							this.label && this.label.remove();
+							this.attr("r", 4);
+						}
+					);
+				})(i, j)
+			}
+		}
+	}
+	
+	var r = this;
 	r.g.txtattr.font = "12px 'Fontin Sans', Fontin-Sans, sans-serif";
 
 	var width = opts.width || 600,
@@ -169,16 +170,18 @@ function drawChart(holder, opts) {
 		values.push([opts.lowValue]);
 	}
 	
-	if (opts.title) {
-		r.g.text((width+paddingLeft)/2, paddingTop, opts.title);
-	}
-	
 	chart = r.g.linechart(paddingLeft, paddingTop, width, height, time, values, {shade: true, nostroke: false, axis: "0 0 1 1", symbol: "o", smooth: true});
 	chart.symbols.attr({r: 4});
-	chart.div = $("#" + holder)[0]
+	chart.div = r.canvas.parentNode;
 	
 	chart.xValues = time;
 	chart.yValues = values;
+	
+	if (opts.title) {
+		title = r.g.text((width+paddingLeft)/2, paddingTop, opts.title);
+		chart.title = title;
+		chart.push(title);
+	}
 	
 	// remove highValue and lowValue graphics
 	for (var i = chartsNumber; i < values.length; i++) {
@@ -194,8 +197,6 @@ function drawChart(holder, opts) {
 	setSymbolCallbacks(chart, time, values, units);
 	overwriteAxis(chart.axis[0]);
 	drawLegend(15, height + 45, {chart: chart, labels: opts.labels, title: opts.legendTitle});
-	
-	chart.selection(function (sel) {console && console.log(Date.formatted_time(sel.endValue()))});
 	
 	return chart;
 }
